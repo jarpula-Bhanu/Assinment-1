67060b09c1f927fa799fbe3fd690d478a7ef92f5
jarpula-Bhanu <102509069+jarpula-Bhanu@users.noreply.github.com>
2022-04-01 22:35:55 +0530

    Add files via upload

--- codes/quardratic.py
+++ codes/quardratic.py
@@ -1,1 +1,32 @@
+#This code is to display the graph and roots of the quadratic equation
+
+from ast import increment_lineno
+import numpy as np
+from scipy.optimize import fsolve
+import matplotlib.pyplot as plt
+import inline 
+
+def f(x):
+    return x**2-4*x-8
+
+x=np.linspace(-3,7)
+y=f(x)
+
+result1 = fsolve(f,6)
+result2 = fsolve(f,-3)
+
+print('the roots of the equations are')
+print(result1)
+print(result2)
+
+plt.grid()
+plt.plot(x,y)
+plt.plot(result1,f(result1),'ko')
+plt.plot(result2,f(result2),'ko')
+plt.title('Plotting of f(x)=$x^2-4x-8$')
+alpha=format(float(result1),".8f")
+beta=format(float(result2),".8f")
+plt.annotate((alpha,0),(result1,f(result1)),size=10,color='grey')
+plt.annotate((beta,0),(result2,f(result2)),size=10,color='grey')
+plt.show()
 
---
+++ codes/roots.c
@@ -0,0 +1,23 @@
+//This code is to print the roots of the equation upto 3 significant digits
+
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+    /*The quardratic equation is
+        x^2-4x-8=0 int the form ax^2+bx+c=0
+        roots are given by  "(-b+sqrt(b^2-4ac))/2a" and "(-b-sqrt(b^2-4ac))/2a"
+    */
+   int a=1,b=-4,c=-8;
+
+   float root1 ,root2;
+
+   root1=(-b+pow(b*b-4*a*c,0.5))/2*a;
+   root2=(-b-pow(b*b-4*a*c,0.5))/2*a;
+
+   printf("The roots of equation are\n");
+   printf("%f and %f",root1,root2);
+
+     
+    return 0;
+}
