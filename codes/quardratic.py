f332585f3c0018fdc1b51500727797fce95d2c19
jarpula-Bhanu <102509069+jarpula-Bhanu@users.noreply.github.com>
2022-04-02 11:04:17 +0530

    Update quardratic.py

--- codes/quardratic.py
+++ codes/quardratic.py
@@ -1,32 +1,20 @@
-#This code is to display the graph and roots of the quadratic equation
-
-from ast import increment_lineno
 import numpy as np
-from scipy.optimize import fsolve
 import matplotlib.pyplot as plt
-import inline 
 
-def f(x):
-    return x**2-4*x-8
+coeff=[1,-4,-8]
+print('The roots of the equation are ')
+print(np.roots(coeff))
 
 x=np.linspace(-3,7)
-y=f(x)
-
-result1 = fsolve(f,6)
-result2 = fsolve(f,-3)
-
-print('the roots of the equations are')
-print(result1)
-print(result2)
+y=x**2-4*x-8
 
 plt.grid()
 plt.plot(x,y)
-plt.plot(result1,f(result1),'ko')
-plt.plot(result2,f(result2),'ko')
+plt.plot(np.roots(coeff)[0],0,'ko')
+plt.plot(np.roots(coeff)[1],0,'ko')
 plt.title('Plotting of f(x)=$x^2-4x-8$')
-alpha=format(float(result1),".8f")
-beta=format(float(result2),".8f")
-plt.annotate((alpha,0),(result1,f(result1)),size=10,color='grey')
-plt.annotate((beta,0),(result2,f(result2)),size=10,color='grey')
+alpha=format(float(np.roots(coeff)[0]),".8f")
+beta=format(float(np.roots(coeff)[1]),".8f")
+plt.annotate((alpha,0),(np.roots(coeff)[0],0),size=10,color='grey')
+plt.annotate((beta,0),(np.roots(coeff)[1],0),size=10,color='grey')
 plt.show()
-
